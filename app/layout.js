221654b9a3963848ecf6d47b8f5f2721b61f947c
Kenn <keaneathallah960@gmail.com>
2025-05-21 19:06:37 +0800

    fix route for logo

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/princess.png" />
+        <link rel="icon" href="/pincess-diana.github.io/princess.png" />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
